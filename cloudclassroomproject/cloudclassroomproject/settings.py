06ca3f1201890e2d4a86000b2c1bc575a0d76411
Ethan Hu <ethanhu8351@gmail.com>
2022-04-07 00:06:12 -0700

    signup function step 1
    user is now added to the db and viewable from admin

--- cloudclassroomproject/cloudclassroomproject/settings.py
+++ cloudclassroomproject/cloudclassroomproject/settings.py
@@ -131,3 +131,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# CRISPY_TEMPLATE_PACK = "uni_form"
--- cloudclassroomproject/main/urls.py
+++ cloudclassroomproject/main/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("signup/", views.signup, name="signup"),
     path("<str:place>", views.general, name="general"),
 ]
