c3b2071cb67c5c45dd8cc4cf938b1760883b0db2
Ethan Hu <ethanhu8351@gmail.com>
2022-05-19 23:17:45 -0700

    modify

--- cloudclassroomproject/cloudclassroomproject/settings.py
+++ cloudclassroomproject/cloudclassroomproject/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-9g$$w%(o&))5=yq4!egi%zyih+0f8*!o277$p7*2a_fs=%)us!"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
